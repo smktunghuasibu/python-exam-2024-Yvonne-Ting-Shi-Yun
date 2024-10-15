74576a26c83f5dcc1c5719fa85cae9c342b546af
Yvonne-Ting-Shi-Yun <163949881+Yvonne-Ting-Shi-Yun@users.noreply.github.com>
2024-10-15 09:01:26 +0800

    Update question_4_min_max.py 4

--- question_4_min_max.py
+++ question_4_min_max.py
@@ -5,16 +5,16 @@
 
 # max_value() is a function that will return maximum number.
 def min_value(a, b, c, d):
-    return ......... (a, b, c, d)
+    return min (a, b, c, d)
 
 def max_value(a, b, c, d):
-    return ......... (a, b, c, d)
+    return max (a, b, c, d)
 
 def main():
-    min = ......... (30.34, 22.23, 48.89, 27.49)
-    max = ......... (30.34, 22.23, 48.89, 27.49)    
-    print(f"Minimum is { ......... }")
-    print(f"Maximum is { ......... }")
+    min = min_value (30.34, 22.23, 48.89, 27.49)
+    max = max_value (30.34, 22.23, 48.89, 27.49)    
+    print(f"Minimum is { min }")
+    print(f"Maximum is { max }")
 
 # Don't change the code below!
 if __name__ == "__main__":
