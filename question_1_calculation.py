93d344070d6e7a10bd70b729504778d6f82ffa83
Yvonne-Ting-Shi-Yun <163949881+Yvonne-Ting-Shi-Yun@users.noreply.github.com>
2024-10-15 08:30:10 +0800

    Update question_1_calculation.py 1

--- question_1_calculation.py
+++ question_1_calculation.py
@@ -4,20 +4,20 @@
 # This program is used to perform basic mathematical calculations.
 
 def calculation(x, y):
-    subtraction = .........
-    multiplication = .........
-    division = ......... if y != 0 else None  # To handle division by zero
+    subtraction = x - y
+    multiplication = x * y
+    division = x / y if y != 0 else None  # To handle division by zero
     return subtraction, multiplication, division
 
 def get_numbers():
     A = float(input("Input A: "))
     B = float(input("Input B: "))    
-    return .........
+    return A, B
 
 def main():
     [x, y] = get_numbers()
-    [S, T, U] = calculation(.........)
-    print(f"subtraction = {.........}, multiplication = {.........}, division = {.........}") 
+    [S, T, U] = calculation(x, y)
+    print(f"subtraction = {S}, multiplication = {T}, division = {U}") 
 
 if __name__ == "__main__":
     main()
