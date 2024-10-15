a8c00f3f201dc7d1025f56ecc57909615a23dc22
Yvonne-Ting-Shi-Yun <163949881+Yvonne-Ting-Shi-Yun@users.noreply.github.com>
2024-10-15 09:15:51 +0800

    Update question_5_compound_interest.py 5

--- question_5_compound_interest.py
+++ question_5_compound_interest.py
@@ -9,20 +9,20 @@
 # (d) number of periods the interest is compounded per year (n)
 
 def cal_matured_value(p, r, t, n):
-    result = P * (Pow(1 + ((r / 100) / t), (n * T) / 12 )) 
-    return Result 
+    result = p * (1 + (r / 100) / n) ** (n * t)  
+    return result 
 
 def get_inputs():
     p = float(input("Enter the principal amount: ")) 
     r = float(input("Enter the interest rate: ")) 
     t = float(input("Enter the time in years: ")) 
     n = float(input("Enter the number of periods the interest is compounded per year: ")) 
-    return (P, R, T, N) 
+    return (p, r, t, n) 
     
 def main():
-    P, R, T, N = get_inputs(P, r, t, n)
-    matured_value = cal_mature_value(P, R, T, N)
-    print(f"Matured value is { mature_value :.2f}") 
+    P, R, T, N = get_inputs()
+    matured_value = cal_matured_value(P, R, T, N)
+    print(f"Matured value is { matured_value :.2f}") 
 
 # Don't change the code below!
 if __name__ == "__main__":
